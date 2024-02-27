7232bd0f7efa6ff63e3acc84e72e4034079823d6
Marko Soldo <150475165+markosoldo@users.noreply.github.com>
2024-02-27 23:59:31 +0100

    Add drawing functionality

--- script.js
+++ script.js
@@ -1,46 +1,103 @@
-const numGridElements = 25;
-
+const customRange = document.querySelector("#customRange");
+const rangeValue = document.querySelector("#rangeValue");
 const drawWindow = document.querySelector("#drawWindow");
+const gridToggle = document.querySelector("#gridToggle");
+const colorToggle = document.querySelector("#colorToggle");
+const rainbowToggle = document.querySelector("#rainbowToggle");
+const eraserToggle = document.querySelector("#eraserToggle");
+const colorPicker = document.querySelector("#colorPicker");
+const clearBtn = document.querySelector("#clearBtn");
 
-for (let i = 0; i < numGridElements; i++)
-  for (j = 0; j < numGridElements; j++) {
+const startNumGridElements = 25;
+const flexBasis = 100 / startNumGridElements;
+const children = drawWindow.children;
+const toggleModes = [colorToggle, rainbowToggle, eraserToggle];
+
+customRange.addEventListener("input", handleRange);
+gridToggle.addEventListener("change", handleGridToggle);
+clearBtn.addEventListener("click", clearGrid);
+
+toggleModes.forEach((toggle) =>
+  toggle.addEventListener("change", handleModeToggle)
+);
+
+for (let i = 0; i < startNumGridElements; i++)
+  for (j = 0; j < startNumGridElements; j++) {
     const newDiv = document.createElement("div");
-    newDiv.classList.add("grid-border");
+    if (gridToggle.checked) newDiv.classList.add("grid-border");
+    newDiv.classList.add("grid-element");
 
+    newDiv.addEventListener("mouseover", fillGrid);
+    newDiv.addEventListener("mousedown", fillGrid);
     drawWindow.appendChild(newDiv);
   }
-
-const children = drawWindow.children;
-
-const flexBasis = 100 / numGridElements;
-
 for (let i = 0; i < children.length; i++) {
   children[i].style.flexBasis = flexBasis + "%";
 }
 
-const customRange = document.querySelector("#customRange");
-const rangeValue = document.querySelector("#rangeValue");
-
-customRange.addEventListener("input", handleRange);
-
 function handleRange() {
-  rangeValue.innerHTML = customRange.value + " X " + customRange.value;
+  rangeValue.innerHTML = this.value + " X " + this.value;
 
   while (drawWindow.firstChild) drawWindow.removeChild(drawWindow.firstChild);
 
-  for (let i = 0; i < customRange.value; i++)
-    for (j = 0; j < customRange.value; j++) {
+  for (let i = 0; i < this.value; i++)
+    for (j = 0; j < this.value; j++) {
       const newDiv = document.createElement("div");
-      newDiv.classList.add("grid-border");
+      if (gridToggle.checked) newDiv.classList.add("grid-border");
+      newDiv.classList.add("grid-element");
 
+      newDiv.addEventListener("mouseover", fillGrid);
+      newDiv.addEventListener("mousedown", fillGrid);
       drawWindow.appendChild(newDiv);
     }
 
   const children = drawWindow.children;
 
-  const flexBasis = 100 / customRange.value;
+  const flexBasis = 100 / this.value;
 
   for (let i = 0; i < children.length; i++) {
     children[i].style.flexBasis = flexBasis + "%";
+  }
+}
+
+function handleGridToggle() {
+  if (this.checked) {
+    for (let i = 0; i < children.length; i++) {
+      children[i].classList.add("grid-border");
+    }
+  } else {
+    for (let i = 0; i < children.length; i++) {
+      children[i].classList.remove("grid-border");
+    }
+  }
+}
+
+function handleModeToggle() {
+  toggleModes.forEach((toggle) => {
+    if (toggle.name !== this.name) toggle.checked = false;
+  });
+}
+
+function fillGrid(event) {
+  if (
+    (event.type === "mouseover" && event.buttons !== 1) ||
+    event.button === 1 ||
+    event.button === 2
+  )
+    return;
+  if (colorToggle.checked) {
+    const color = colorPicker.value;
+    event.target.style.backgroundColor = color;
+  } else if (rainbowToggle.checked) {
+    const color = `hsl(${Math.floor(Math.random() * 360)}, 100%, 50%)`;
+    event.target.style.backgroundColor = color;
+  } else if (eraserToggle.checked) {
+    event.target.style.backgroundColor = "white";
+  }
+}
+
+function clearGrid() {
+  for (let i = 0; i < children.length; i++) {
+    children[i].style.backgroundColor = "white";
   }
 }
